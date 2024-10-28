bfb40ae68068e081f8fae249a6c758b1e7e0f53f
Purushothaman-A <purushothaman.arm@gmail.com>
2024-10-28 13:12:41 +0530

    Create Main.java

---
+++ ATM Project/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+// Main class to run the ATM project
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        OptionMenu opt = new OptionMenu();
+        Account account = new Account();
+
+        System.out.println("Welcome to the ATM Project!");
+
+        int attempts =0;
+        final int maxAttempts =3;
+
+        while (attempts<maxAttempts) { // Loop until valid credentials are entered
+            System.out.print("Enter Your Customer Number: ");
+            int cNo = scanner.nextInt();
+            System.out.print("Enter Your Pin Number: ");
+            int pNo = scanner.nextInt();
+
+            // Validate credentials
+            if (cNo == 1234 && pNo == 12345 || cNo == 4321 && pNo == 54321) {
+                opt.account(); // Access account options if credentials are valid
+                break; // Exit the loop on successful login
+            } else {
+                attempts++;
+                System.out.println("Incorrect Pin or Account Number. Please try again."); // Prompt for re-entry
+                if(attempts>=maxAttempts){
+                    System.out.println("Maximum Attempts Reached .Exiting");
+                }
+            }
+        }
+    }
+}
